717dca27a28b01fadbf5501e209323c79163e56d
gatraenggar <gatraenggarwicaksono@gmail.com>
2022-08-17 14:30:44 +0800

    feat: add user join event

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
-const port = 3000
-
-io.on("connection", (socket) => {
-  console.log("Connected!")
-  // ...
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
 });
 
-console.log("Server running on http://localhost:" + port)
+const port = process.env.PORT || 3001;
 
+const userList = [];
+
+io.on("connection", (socket) => {
+  console.log('A user connected with ID: ' + socket.id);
+  console.log('Total users: ' + userList.length);
+
+  socket.on('user-join', ({ username }) => {
+    userList.push({
+      id: socket.id,
+      username: username,
+    });
+
+    socket.emit('user-list', userList);
+    console.log('user-list emitted')
+  });
+
+});
+
+console.log("Server running on http://localhost:" + port);
 httpServer.listen(port);
