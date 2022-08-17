dd5b8f213a276711f8c3e6d94a79608dce00a696
gatraenggar <gatraenggarwicaksono@gmail.com>
2022-08-17 15:59:54 +0800

    feat: handle event when friend leave

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const io = new Server(httpServer, {
 
 const port = process.env.PORT || 3001;
 
+const room = "spywerk";
 const userList = [];
 
 io.on("connection", (socket) => {
   console.log('A user connected with ID: ' + socket.id);
-  console.log('Total users: ' + userList.length);
 
   socket.on('user-join', ({ username }) => {
     userList.push({
@@ -24,10 +24,28 @@ io.on("connection", (socket) => {
       username: username,
     });
 
+    socket.join(room);
+    console.log('Total users: ' + userList.length);
+
+    io.in(room).emit('friend-join', userList);
+
     socket.emit('user-list', userList);
-    console.log('user-list emitted')
+    console.log('user-list emitted');
   });
 
+  socket.on('disconnect', () => {
+    const i = userList.findIndex((user) => user.id === socket.id);
+
+    if (i !== -1) {
+      let username = userList[i].username;
+      userList.splice(i, 1);
+
+      socket.to(room).emit('friend-leave', {
+        userList,
+        message: username + ' left the game',
+      });
+    }
+  });
 });
 
 console.log("Server running on http://localhost:" + port);
