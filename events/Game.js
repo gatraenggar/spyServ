ca1510e73ab4d35415c8b2d89cf4a0acebdccfee
gatraenggar <gatraenggarwicaksono@gmail.com>
2022-08-20 15:28:52 +0800

    refactor: event handlers to be modular

---
+++ events/Game.js
@@ -0,0 +1,33 @@
+class Game {
+  constructor (socket, io, userList, places) {
+    this.socket = socket
+    this.io = io
+    this.userList = userList
+    this.places = places
+  }
+
+  start(room) {
+    this.socket.on('game-start', () => {
+      let placeIndex, spy, spyIndex
+
+      // return random int between 0 to (length-1)
+      placeIndex = Math.floor(Math.random() * this.places.length);
+      spyIndex = Math.floor(Math.random() * this.userList.length);
+  
+      spy = this.userList[spyIndex].username
+      const roles = this.places[placeIndex].roles
+  
+      this.userList.forEach((user, i) => (
+        this.userList[i] = {
+        id: user.id,
+        username: user.username,
+        role: user.username === spy ? "Spy" : roles[Math.floor(Math.random() * roles.length)],
+        place: user.username === spy ? "?" : this.places[placeIndex].name
+      }))
+  
+      this.io.in(room).emit('role-assign', { userList: this.userList });
+    })
+  }
+}
+
+module.exports = Game
